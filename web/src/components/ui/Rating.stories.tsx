7127f11b767b06a36a0438c4389bb87c7796935a
Noah Perkins <61742492+Noupin@users.noreply.github.com>
2025-03-02 18:49:51 -0500

    adding components to storybook

---
+++ web/src/components/ui/Rating.stories.tsx
@@ -0,0 +1,13 @@
+import type { Meta, StoryObj } from '@storybook/react'
+
+import { Rating } from './Rating'
+
+const meta: Meta<typeof Rating> = {
+  component: Rating,
+}
+
+export default meta
+
+type Story = StoryObj<typeof Rating>
+
+export const Primary: Story = {}
